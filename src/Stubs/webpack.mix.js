b33ff5f7b5a062bc8adef8312f1f072f66510699
mikehins <mike@hins.dev>
2022-09-20 10:19:36 -0400

    Add stubs

--- src/Stubs/webpack.mix.js
+++ src/Stubs/webpack.mix.js
@@ -4,17 +4,14 @@ require('laravel-mix-blade-reload');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .bladeReload();
+    .bladeReload()
 
 // ALL PAGES SEPERATLY
-fs.readdirSync('resources/assets/js/pages')
-    .filter(p => /\.js$/.test(p))
+fs.readdirSync('resources/assets/js/pages').filter(p => /\.js$/.test(p))
     .forEach(p => mix.js('resources/assets/js/pages/' + p, 'public/assets/js/pages'))
 
-fs.readdirSync('resources/assets/sass/pages')
-    .filter(p => /\.scss$/.test(p))
+fs.readdirSync('resources/assets/sass/pages').filter(p => /\.scss$/.test(p))
     .forEach(p => mix.sass('resources/assets/sass/pages/' + p, 'public/assets/css/pages'))
-
 
 if (!mix.inProduction()) {
     mix.webpackConfig({
